1b8f684bdf51b36ade3010ec89f4704b9c745a72
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-04-30 22:56:07 -0400

    edited logo

--- apps.js
+++ apps.js
@@ -40,4 +40,6 @@ gapi.client.load('calendar', 'v3', function () { // load the calendar api (versi
 
                    "resource": resource 	// above resource will be passed here
                });                
-}
+  }
+
+  
